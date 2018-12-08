ba3cb940994250addfd15fb83fcc41fba277cea8
AnastasiaTyvonuk <ntyvonuk08@gmail.com>
2018-12-08 23:00:03 +0200

    Added task4.

---
+++ task4/Task4/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Configuration;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Task task = new Task();
+            task.DoTasks();
+
+            Console.ReadKey();
+        }
+    }
+}
